cb7af9f91b3f64c5088a2cc72bfd83a8f6504c3d
Guillaume Marcel <guillaume.k.marcel@gmail.com>
2021-06-07 10:54:59 +0200

    set image upload to specific media dir

--- photoweb/settings.py
+++ photoweb/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os.path
 
 from pathlib import Path
 
@@ -128,3 +129,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = (                                                                                                                                                     
     'website/static',
 )
+
+# Media
+
+# Base url to serve media files
+MEDIA_URL = '/media/'
+
+# Path where media is stored
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- website/models.py
+++ website/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
+from .apps import WebsiteConfig
+
 
 class Photo(models.Model):
-    image = models.ImageField(upload_to='images')
-    dt_shot = models.DateField(default=None)
+    image = models.ImageField(upload_to=f'{WebsiteConfig.name}/images/')
+    dt_shot = models.DateField(default=None, blank=True, null=True)
