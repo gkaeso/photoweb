0009fc574065324e60ea784632e5d603bd7dd1d7
Guillaume Marcel <guillaume.k.marcel@gmail.com>
2021-06-07 09:40:43 +0200

    add model Photo + change time zone

--- website/models.py
+++ website/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='images')
+    dt_shot = models.DateField(default=None)
